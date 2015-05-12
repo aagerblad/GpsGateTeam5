3157d4b2fae352e1cd14caebd10e5de9a2e74865
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-05-12 18:18:33 +0200

    wat

---
+++ geolocation.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+	GpsGate.Server.Hackathon.ReportPosition("t5test", "foo", "yooooo", "t5test", 0, 0);
+/*	navigator.geolocation.getCurrentPosition(function (position) {
+		console.log(position);
+  });
+*/});
